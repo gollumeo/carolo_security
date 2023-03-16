bf7f7ddeb2374c33ac07784326d35e93c2974bd1
Jonathan Manes <manesjona@gmail.com>
2023-03-16 15:46:32 +0100

    Refactoring

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,13 @@
 package com.example.demo;
 
-import org.bytedeco.javacv.*;
-import org.bytedeco.opencv.opencv_core.IplImage;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-
-import javax.swing.*;
-import java.awt.event.WindowEvent;
-import java.util.UUID;
-
-import static org.bytedeco.opencv.helper.opencv_imgcodecs.cvSaveImage;
 
 @EnableScheduling
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -25,5 +15,6 @@ public class DemoApplication {
 
         builder.headless(false);
 
-        ConfigurableApplicationContext context = builder.run(args);    }
+        ConfigurableApplicationContext context = builder.run(args);
+    }
 }
